ad4aab2db668d0f286bd55e21abc6d68691a9cc7
Maxim Toma <maxim.toma@sky.uk>
2023-11-07 16:22:50 +0000

    Created app Post, Put, and Delete endpoints

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 //TODO: Create your GET Request Route Below:
 
@@ -17,6 +17,32 @@ app.get("/restaurants/:id", async (req, res) => {
   const theId = req.params.id;
   const theRecord = await Restaurant.findByPk(theId);
   res.json(theRecord);
+});
+
+app.post("/restaurants", async (req, res) => {
+  console.log(req.body);
+
+  const newRest = await Restaurant.create(req.body);
+  res.send(newRest);
+
+  console.log("Post works!");
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  const theId = req.params.id;
+  const result = await Restaurant.update(req.body, { where: { id: theId } });
+  res.json(result);
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  const theId = req.params.id;
+  const result = await Restaurant.destroy({ where: { id: theId } });
+
+  if (result === 1) {
+    res.send("Successfully deleted the record!");
+  } else {
+    res.send("No such record found");
+  }
 });
 
 module.exports = app;

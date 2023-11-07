0566d0a02e33d88c0621093ddad0274ba9b14d29
Maxim Toma <maxim.toma@sky.uk>
2023-11-07 14:30:13 +0000

    Created GET endpoint with id parameter

--- src/app.js
+++ src/app.js
@@ -3,11 +3,20 @@ const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 //TODO: Create your GET Request Route Below:
 
 app.get("/restaurants", async (req, res) => {
   const allRest = await Restaurant.findAll();
   res.json(allRest);
+});
+
+app.get("/restaurants/:id", async (req, res) => {
+  const theId = req.params.id;
+  const theRecord = await Restaurant.findByPk(theId);
+  res.json(theRecord);
 });
 
 module.exports = app;
